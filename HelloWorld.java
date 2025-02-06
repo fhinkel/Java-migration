0d7c5e21e36e83bd535c2beca0c50e36d0540777
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-02-05 22:58:59 -0500

    Add switch

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,14 @@ import java.util.List;
 
 public class HelloWorld {
 
+    private static final String MONDAY = "Monday";
+    private static final String TUESDAY = "Tuesday";
+    private static final String WEDNESDAY = "Wednesday";
+    private static final String THURSDAY = "Thursday";
+    private static final String FRIDAY = "Friday";
+    private static final String SATURDAY = "Saturday";
+    private static final String SUNDAY = "Sunday";
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
@@ -12,12 +20,28 @@ public class HelloWorld {
         names.add("Charlie");
 
         // for (String name : names) {
-        //     System.out.println("Hello, " + name + "!");
+        // System.out.println("Hello, " + name + "!");
         // }
 
         for (int i = 0; i < names.size(); i++) {
-            System.out.println("Hello, " + names.get(i)+ "!");
+            System.out.println("Hello, " + names.get(i) + "!");
         }
 
+        String day = MONDAY;
+
+        String result = "";
+        switch (day) {
+            case MONDAY:
+                result = "Have a good start of the workweek!";
+                break;
+            case FRIDAY:
+                result = "FriYay!";
+                break;
+            case SATURDAY:
+                result = "Enjoy your weekend!";
+                break;
+        }
+
+        System.out.println(result);
     }
 }
