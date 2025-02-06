6e229e1cfcd19d9d83e850361a347b8cc286c7ca
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-02-05 22:51:39 -0500

    Add for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,15 +4,19 @@ import java.util.List;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
 
         List<String> names = new ArrayList<>();
         names.add("Alice");
         names.add("Bob");
         names.add("Charlie");
 
-        for (String name : names) {
-            System.out.println("Hello, " + name + "!");
+        // for (String name : names) {
+        //     System.out.println("Hello, " + name + "!");
+        // }
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println("Hello, " + names.get(i)+ "!");
         }
 
     }
