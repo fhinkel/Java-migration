9c4e524bd7e54b3ffba6ecd0ea3ea84700b8ee83
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-02-05 23:54:45 -0500

    All in one file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
+class Person {
+
+    private final String name;
+    private final String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (!(obj instanceof Person)) {
+            return false;
+        } else {
+            Person other = (Person) obj;
+            return Objects.equals(name, other.name)
+                    && Objects.equals(address, other.address);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", address=" + address + "]";
+    }
+
+    // standard getters
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+}
 
 public class HelloWorld {
 
@@ -43,7 +88,12 @@ public class HelloWorld {
         var thirdPerson = new Person("Jane Doe", "100 Linda Ln.");
 
         if (person.equals(secondPerson)) {
-            System.out.println(person.getName() + " and " + secondPerson.getName() + " are the same person."); // Accessor methods are getName() and address()
+            System.out.println(person.getName() + " and " + secondPerson.getName() + " are the same person."); // Accessor
+                                                                                                               // methods
+                                                                                                               // are
+                                                                                                               // getName()
+                                                                                                               // and
+                                                                                                               // address()
         }
 
         if (!person.equals(thirdPerson)) {
@@ -51,4 +101,3 @@ public class HelloWorld {
         }
     }
 }
-
