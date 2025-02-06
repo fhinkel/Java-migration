07f9a699559d5271bfb83cb0331ef9c6faac9a4c
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2025-02-05 23:11:54 -0500

    The next step is to replace the hard coded day with a today() function.
    Prompt: write a java funtion that returns today's date

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,8 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HelloWorld {
-
-    private static final String MONDAY = "Monday";
-    private static final String TUESDAY = "Tuesday";
-    private static final String WEDNESDAY = "Wednesday";
-    private static final String THURSDAY = "Thursday";
-    private static final String FRIDAY = "Friday";
-    private static final String SATURDAY = "Saturday";
-    private static final String SUNDAY = "Sunday";
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
@@ -22,21 +15,29 @@ public class HelloWorld {
         names.add("Eve");
 
         // Enhanced for loop (more idiomatic, even though not a Java 21 specific change)
-        for (var name : names) {       // var makes sense here
+        for (var name : names) { // var makes sense here
             System.out.println("Hello, " + name + "!");
         }
 
+        var day = today();
+        System.out.println("Today is " + day);
 
-        var day = MONDAY;             // var
 
         // Enhanced switch (Java 14+, preferred style)
-        var result = switch (day) {   // var
-            case MONDAY -> "Have a good start of the workweek!";
-            case FRIDAY -> "FriYay!";
-            case SATURDAY -> "Enjoy your weekend!";
-            default -> ""; // Always have a default branch or exhaustive cases
+        var result = switch (day) { // var
+            case "MONDAY" -> "Have a good start of the workweek!";
+            case "WEDNESDAY" -> "Hump day, working towards the weekend.";
+            case "FRIDAY" -> "FriYay!";
+            case "SATURDAY" -> "Enjoy your weekend!";
+            default -> "It's a normal day, " + day + "."; // Always have a default branch or exhaustive cases
         };
 
         System.out.println(result);
     }
+
+    private static String today() {
+        LocalDate today = LocalDate.now();
+        return today.getDayOfWeek().toString(); // Returns "MONDAY", "TUESDAY", etc.
+    }
+
 }
